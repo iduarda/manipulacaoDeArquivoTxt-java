bc11e824333cb4916f80dcaa4b514d77cfb63823
Maria Eduarda <142428634+iduarda@users.noreply.github.com>
2024-03-25 18:01:59 -0300

    case 1 adicionada

--- manipulacaoDeArquivoTxt/src/manipulacaoDeArquivoTxt/Principal.java
+++ manipulacaoDeArquivoTxt/src/manipulacaoDeArquivoTxt/Principal.java
@@ -1,32 +1,54 @@
 package manipulacaoDeArquivoTxt;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner teclado = new Scanner(System.in);
 		int loop = 1;
 		
-		while (loop == 1) {
+		while(loop == 1) {
+		
             System.out.println("\nMenu:");
             System.out.println("1 - Inserir nome no arquivo");
             System.out.println("2 - Exibir todos os nomes do arquivo");
             System.out.println("3 - Apagar todo arquivo");
+            System.out.println("4 - Sair");
             System.out.print("Escolha uma opção: ");
             int op = teclado.nextInt();
             
             switch(op) {
             case 1:{
-            	Opcoes.opcao1();
+            	Path path = Path.of("C:\\Users\\mrobe\\eclipse-workspace\\manipulacaoDeArquivoTxt\\texto.txt");
+        		
+        		if(Files.notExists(path)) {
+        			Files.createFile(path);
+        		}
+        		
+        		Scanner teclado1 = new Scanner(System.in);
+        		String texto;
+        		
+        		System.out.println("Escreva o que você deseja inserir no arquivo: ");
+        		texto = teclado1.nextLine();
+        		
+        		Files.writeString(path, texto);
             	break;
             }
             case 2:{
-            	Opcoes.opcao2();
+            	
             	break;
             }
             case 3:{
-            	Opcoes.opcao3();
+            	
+            	break;
+            }
+            case 4:{
+            	loop = 0;
+            	System.out.println("Saindo...");
             	break;
             }
             default:{
@@ -35,11 +57,8 @@ public class Principal {
             }
             }
             
-            if(loop != 0) {
-            	System.out.println("\nDigite '1' para voltar ao menu, ou '0' para encerrar o programa.");
-            	loop = teclado.nextInt();
-            }
 		}
 	teclado.close();
 	}
+
 }
